6fbf1b9ce5040b3cbe449b77503d01a7ffa5dd83
Jimmy Ko <ko.jimmy09@gmail.com>
2024-07-12 18:04:33 -0400

    move terraform files to project root

--- terraform/ecr.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "my-tf-images" {
-  name         = "${var.app_name}-images"
-  force_delete = true
-}
--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ecr_repo_url" {
-  value = aws_ecr_repository.my-tf-images.repository_url
-}
-
-output "alb_dns" {
-  value = aws_alb.my-tf-alb.dns_name
-}
