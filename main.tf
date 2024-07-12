e0942574b8f3a46737d4b365afc66ea1145d40fe
Jimmy Ko <ko.jimmy09@gmail.com>
2024-07-12 18:06:14 -0400

    move terraform files to project root

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  # required_providers {
+  #   aws = {
+  #     source  = "hashicorp/aws"
+  #     version = "~>5.56"
+  #   }
+  # }
+
+  backend "remote" {
+    organization = "deploy-in-public"
+
+    workspaces {
+      name = "deploy-in-public"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source             = "./vpc"
+  app_name           = var.app_name
+  vpc_cidr_block     = var.vpc_cidr_block
+  public_subnet_ids  = var.public_subnet_ids
+  private_subnet_ids = var.private_subnet_ids
+}
