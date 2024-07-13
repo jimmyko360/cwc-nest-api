a4f9d40fdce92226fa52e797a50371e088cfabb5
Jimmy Ko <ko.jimmy09@gmail.com>
2024-07-13 00:16:58 -0400

    remove aws cli env vars

--- variables.tf
+++ variables.tf
@@ -35,10 +35,10 @@ variable "instance_data" {
   }
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  type = string
-}
+# variable "AWS_ACCESS_KEY_ID" {
+#   type = string
+# }
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
-}
+# variable "AWS_SECRET_ACCESS_KEY" {
+#   type = string
+# }
