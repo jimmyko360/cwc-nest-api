544b4e4c4722290223d8c924b82214254ebd51f8
Jimmy Ko <ko.jimmy09@gmail.com>
2024-07-13 03:02:37 -0400

    save ec2 ids into 2 env vars

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "alb_dns" {
   value = aws_alb.my-tf-alb.dns_name
 }
 
-output "ec2_ids" {
-  value = aws_instance.my-tf-instances.*.id
+output "instance_1_ID" {
+  value = aws_instance.my-tf-instances[0].id
+}
+
+output "instance_2_ID" {
+  value = aws_instance.my-tf-instances[1].id
 }
